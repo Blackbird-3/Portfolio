e4430ab749b3f9e993e60ac4865b4a5aeb17999e
Shreshtth Kumar Agarwaal <agarwalshreshth3@gmail.com>
2025-04-29 00:10:29 +0530

    spline working

---
+++ shreshtth-portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from './sections/Hero'
+
+
+function App() {
+
+  return (
+    <>
+      <Hero />
+    </>
+  )
+}
+
+export default App
---
+++ shreshtth-portfolio/src/sections/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Spline from '@splinetool/react-spline';
+
+
+function Hero() {
+  return (
+    <>
+      <div className="flex items-center justify-center flex-col m-10">
+        
+      <div className="name1">SHRESHTTH</div>
+      <div className="name2">KUMAR</div>
+      <div className="name3">AGARWAAL</div>
+      </div>
+    <section id='hero' className="flex items-center justify-center h-screen">
+        <div className="container flex items-center justify-center relative ">  
+        <Spline scene="https://prod.spline.design/OemSZe1Cer3wizE0/scene.splinecode" />
+          <div className="absolute bottom-0 right-0 w-50 h-20 bg-black"></div>
+        </div>  
+        </section>  
+          <div className="random">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad sapiente repudiandae voluptatem, nulla magnam doloribus. Rerum odit enim voluptatum hic quam tempore dicta corporis qui magnam molestiae, minus dolore! Ea aspernatur deserunt eaque explicabo unde voluptatum beatae ducimus nemo esse cumque illum, dicta dolorum neque eligendi tempora incidunt dolor temporibus ut perspiciatis! Temporibus illum repellendus eius saepe distinctio voluptas tenetur ipsam consequuntur et enim, repellat possimus numquam accusamus tempora placeat inventore quam. Culpa dolorum placeat iure harum at debitis ducimus nobis! Quas id iste ullam fuga asperiores ipsa expedita saepe doloribus laboriosam, eos cum mollitia facere sunt laborum fugit deleniti modi nostrum nulla voluptatibus, itaque ducimus ex placeat assumenda? Vel quibusdam quis explicabo, nesciunt ratione eveniet voluptas placeat reiciendis quia enim obcaecati amet eligendi voluptatibus illum non facere sapiente magnam! Optio saepe minus non similique eum at voluptatem corporis nobis in maxime nemo qui illo odit, ad officia nihil, atque facilis sit ipsam. Quis quo earum inventore eligendi, vitae molestiae expedita, facilis facere culpa eveniet accusantium? Accusamus veritatis fugiat molestiae quidem nobis ipsam at officia impedit, voluptate dolor esse sint iure expedita quia corrupti assumenda explicabo placeat tempore aut! Quia impedit omnis doloremque est, pariatur itaque tenetur eum veritatis deleniti rerum explicabo officia neque numquam qui ipsam eius molestiae assumenda soluta alias facilis autem culpa laudantium sit. Modi voluptatum eos a sed possimus, ullam ut necessitatibus pariatur, quod iure obcaecati sunt. Inventore odit velit aliquam molestias nisi suscipit sed asperiores vel eos, laborum error rem totam aspernatur, iusto soluta assumenda. Magni assumenda nulla, nam corrupti eaque ducimus excepturi unde quia dignissimos mollitia fugit? Hic dolore similique aliquam, autem, tempore ipsa officiis quibusdam assumenda eum expedita aspernatur rerum culpa vitae architecto ducimus dolor! Exercitationem ipsam obcaecati, praesentium sequi tempore omnis vel! Aliquam, fuga rerum! Impedit amet sapiente itaque eveniet atque excepturi explicabo accusamus voluptatibus, reiciendis commodi ratione suscipit nobis perspiciatis nesciunt consectetur deleniti veritatis laudantium at nam optio nulla ipsam minima doloribus. Impedit, temporibus odio animi deleniti voluptas velit quibusdam necessitatibus. At quis libero, itaque ipsam sunt soluta rem aut nisi natus perferendis quia pariatur dignissimos sit eos amet corporis aperiam! Commodi a in, laudantium quis numquam natus voluptatem placeat quibusdam itaque dolorem distinctio maiores, perspiciatis ipsum architecto consectetur soluta dolorum quo? Magnam nesciunt minus delectus odio ipsum quis reprehenderit nihil beatae deleniti rerum laboriosam velit fugiat, atque neque aliquid voluptatum sint explicabo, animi nostrum necessitatibus consequatur! Error pariatur eligendi enim!</div>
+    </>
+  )
+}
+
+export default Hero
